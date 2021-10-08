630bac008421f27c7186698288786083ff1dc5e9
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-10-08 15:11:10 +0530

    Update run.sh

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-javac -d out/production/assignment1/ src/com/company/Main.java
-cd out/production/assignment1/
+cd src/
+javac -d ../out/production/assignment1/ com/company/Main.java
+cd ../out/production/assignment1/
 java com.company.Main
 cd ../../..
