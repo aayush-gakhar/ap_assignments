8df0b6b166d27340ee06b362d2549d913f36b218
Aayush Gakhar <aayush20006@iiitd.ac.in>
2021-10-25 19:32:47 +0530

    Update Game.java

--- assignment3/src/com/company/Game.java
+++ assignment3/src/com/company/Game.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,9 +9,11 @@ public class Game {
     private final Dice dice = new Dice(2);
     private final List<Floor> floors = new LinkedList<>();
     private boolean started = false;
+    private final Scanner scanner;
 
-    Game(String name) {
+    Game(String name, Scanner scanner) {
         this.player = new Player(name, this);
+        this.scanner=scanner;
         floors.add(new EmptyFloor());
         floors.add(new EmptyFloor());
         floors.add(new ElevatorFloor());
@@ -32,27 +31,90 @@ public class Game {
         System.out.println("The game setup is ready");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the player name and hit enter");
         String name =scanner.nextLine();
-        Game game = new Game(name);
+        Game game = new Game(name,scanner);
+        game.bonus();
         while (true) {
             System.out.print("Hit enter to roll the dice");
             game.pressEnter();
 //            int roll=scanner.nextInt();
             int roll = game.dice.roll();
             game.playAMove(roll);
-            if (game.player.getPosition() == 13) {
+            if (game.gameOver()) {
                 System.out.println("Game over");
                 System.out.println(game.player + " accumulated " + game.player.getPoints() + " points");
-//                Path fileName = Path.of("/Users/aayushgakhar/Desktop/score.txt");
-//                int max=Integer.parseInt(Files.readString(fileName));
-//                if(game.player.getPoints()>max)
-//                    Files.writeString(fileName,String.valueOf(game.player.getPoints()));
                 break;
             }
         }
+    }
+
+    public void bonus(){
+        System.out.print("Want bonus?[yN]: ");
+        String in=scanner.next().toLowerCase();
+        if(in.equals("n")){
+            return;
+        }else if(in.equals("y")){
+            _bonus();
+        }else {
+            bonus();
+        }
+    }
+
+    public void _bonus(){
+        System.out.println("Add new floors!!!");
+        System.out.print("Enter no of floors you want to add: ");
+        int newFloors=scanner.nextInt();
+        System.out.println("floor menu:\n1.Empty floor\n2.Normal ladder floor\n3.Elevator floor\n4.Normal snake floor\n5.King cobra floor");
+        for (int i = 0; i < newFloors; i++) {
+            int floorId=scanner.nextInt();
+            if(floorId<1||floorId>5){
+                System.out.println("Invalid input");
+                i--;
+                continue;
+            }
+            if(floorId==1){
+                floors.add(new EmptyFloor());
+                System.out.println("Added empty floor");
+            }else if(floorId==2){
+                if(newFloors-i<5){
+                    System.out.println("can't add ladder in last 4 floors");
+                    i--;
+                    continue;
+                }
+                floors.add(new NormalLadderFloor());
+                System.out.println("Added normal ladder floor");
+            }else if(floorId==3){
+                if(newFloors-i<9){
+                    System.out.println("can't add Elevator in last 8 floors");
+                    i--;
+                    continue;
+                }
+                floors.add(new ElevatorFloor());
+                System.out.println("Added elevator floor");
+            }else if(floorId==4){
+                if(floors.get(floors.size()-1).getClass()==NormalSnakeFloor.class ||
+                        floors.get(floors.size()-1).getClass()==KingCobraSnakeFloor.class){
+                    System.out.println("can't add snake floors continuously");
+                    i--;
+                    continue;
+                }
+                floors.add(new NormalSnakeFloor());
+                System.out.println("Added normal snake floor");
+            }else{
+                if(floors.get(floors.size()-1).getClass()==NormalSnakeFloor.class ||
+                        floors.get(floors.size()-1).getClass()==KingCobraSnakeFloor.class){
+                    System.out.println("can't add snake floors continuously");
+                    i--;
+                    continue;
+                }
+                floors.add(new KingCobraSnakeFloor());
+                System.out.println("Added King cobra snake floor");
+            }
+        }
+
     }
 
     public void pressEnter() {
@@ -71,6 +133,10 @@ public class Game {
         this.started = started;
     }
 
+    public boolean gameOver(){
+        return player.getPosition()==floors.size()-1;
+    }
+
     public List<Floor> getFloors() {
         return floors;
     }
@@ -84,7 +150,7 @@ public class Game {
                 return;
             }
         }
-        if (player.getPosition() + roll > 13) {
+        if (player.getPosition() + roll > floors.size()-1) {
             System.out.println("Player cannot move");
             return;
         }
